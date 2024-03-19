96072130e95639da64e3e16a66df0597b07b3387
carlostoni <carlos_toni_azevedo@hotmail.com>
2024-03-19 12:43:10 -0300

    1.2

---
+++ Desafio_1/desafio_2.py
@@ -0,0 +1,4 @@
+#Calculadora de Expressões Matemáticas:
+
+#Desenvolva uma calculadora que seja capaz de avaliar expressões matemáticas complexas.
+#Considere a inclusão de funções trigonométricas e logaritmos.
---
+++ desafio_3.py
@@ -0,0 +1,9 @@
+#Analisador de Texto:
+#Desenvolva um programa que analise um texto e forneça estatísticas, como número de palavras, número de frases, palavras mais frequentes, etc.
+
+
+print('Digite uma frase para começar')
+frase = input()
+
+print(f'a frase contem {len(frase.replace(' ',''))} letras ')
+print(f'a frase contem {len(frase)}')
