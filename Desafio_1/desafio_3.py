384e5a3f694f6a9a7a21b3f9d7891e8c85885c5b
carlostoni <carlos_toni_azevedo@hotmail.com>
2024-03-25 12:44:39 -0300

    1.3.2

--- Desafio_1/desafio_3.py
+++ Desafio_1/desafio_3.py
@@ -8,3 +8,16 @@ frase = input()
 print(f'a frase contem {len(frase.replace(' ','').replace(".", ""))} letras ')
 print(f'a frase contem {len(frase.split( ))} palavras')
 print(f'a frase contem {len(frase.split("."))-1} frases')
+
+palavras = frase.split()
+contador = {}
+for palavra in palavras:
+        if palavra in contador:
+            contador[palavra] += 1
+        else:
+            contador[palavra] = 1
+    
+print(contador)
+
+
+
