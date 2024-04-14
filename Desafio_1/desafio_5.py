0830be5b91b6292fce20d9993e5c4fb4b6ebd4f6
carlostoni <carlos_toni_azevedo@hotmail.com>
2024-04-14 13:12:10 -0300

    1.5

---
+++ Desafio_1/desafio_5.py
@@ -0,0 +1,8 @@
+#Faça um programa que peça um número ao usuário e informe se é Par ou Ímpar.
+
+print(f"Digite uma numero ")
+numero = (int(input()))
+if numero % 2 == 0:
+    print("O Numero e par")
+else:
+    print("O numero e impar")
