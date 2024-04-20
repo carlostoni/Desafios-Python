24f84bfa3b908af8fa807a6d6cc4679e73223709
carlostoni <carlos_toni_azevedo@hotmail.com>
2024-04-20 11:27:25 -0300

    1.9.2

--- Desafios/desafio_9.py
+++ Desafios/desafio_9.py
@@ -3,39 +3,44 @@ class Jogo:
     def __init__(self):
         self.personagem_1 = [1, 2, 3]
         self.personagem_2 = [2, 1, 3]
+        
     def oponente(self):
         self.oponente_1 = [1, 2, 3]
         self.oponente_2 = [2, 1, 3]
 class Regra:
     def __init__(self):
         self.jogo = Jogo()
-
+        self.jogo.oponente()
+        
     def selecionar_personagem(self):
         print('''Selecione um personagem
               1 = Guerreiro
-              2 = personagem_2''')
+              2 = Mago''')
+        
         self.selecao = int(input())
         if self.selecao == 1:
-            personagem_1 = self.jogo.personagem_1
+            self.personagem = self.jogo.personagem_1
             self.nome = "Guerreiro"
-            return personagem_1
+            
         elif self.selecao == 2:
-            personagem_2 = self.jogo.personagem_2
+            self.personagem = self.jogo.personagem_2
             self.nome = "Mago"
-            return personagem_2
+            
         else:
             print("Opcao invalida")
             
     def selecionar_oponente(self):
-        self.jogo.oponente()
         self.lista_escolhida = random.choice([self.jogo.oponente_1, self.jogo.oponente_2])
+        if self.lista_escolhida == self.jogo.oponente_1:
+            self.nome_op = "Vampiro"
+        else:
+            self.nome_op = "Gargula"
         
-        return self.lista_escolhida
 
 # Exemplo de uso:
 regra = Regra()
-jogo = Jogo()
-personagem_selecionado = regra.selecionar_personagem()
-oponente_selecionado = regra.selecionar_oponente()
-print(f"{regra.nome} {personagem_selecionado}")  
-print(oponente_selecionado)
+regra.selecionar_personagem()
+regra.selecionar_oponente()
+
+print(f"{regra.nome} {regra.personagem}")  
+print(f"{regra.nome_op} {regra.lista_escolhida}")
