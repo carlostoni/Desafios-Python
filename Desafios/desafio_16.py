6f37fa9438de99476891ea2632c5b786b7d5dd1f
carlostoni <carlos_toni_azevedo@hotmail.com>
2024-05-15 09:39:13 -0300

    1.16

---
+++ Desafios/desafio_16.py
@@ -0,0 +1,5 @@
+#Faça um jogo de Pedra, Papel e Tesoura.
+#No qual você digitará a letra P para jogar Papel, a letra R para jogar Pedra e a letra T para jogar Tesoura.
+#Você jogará contra o computador e contabilizara o número de vitórias, empates e derrotas.
+
+
