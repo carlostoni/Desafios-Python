9be1215ef1ab3f8c386855ea31a2e5db42497cfb
carlostoni <carlos_toni_azevedo@hotmail.com>
2024-05-21 12:44:37 -0300

    1.16.1

--- Desafios/desafio_16.py
+++ Desafios/desafio_16.py
@@ -2,4 +2,14 @@
 #No qual você digitará a letra P para jogar Papel, a letra R para jogar Pedra e a letra T para jogar Tesoura.
 #Você jogará contra o computador e contabilizara o número de vitórias, empates e derrotas.
 
+pedra = 'pedra'
+papel = 'papel'
+tesoura = 'tesoura'
 
+entrada = input("Insira ")
+
+if entrada == pedra: 
+    if pedra > tesoura:
+        print("venceu")
+    elif pedra < papel:
+        print("perdeu")
