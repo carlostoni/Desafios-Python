d48c36d12b27fadd502237fb6cad27d2bf2a2eda
carlostoni <carlos_toni_azevedo@hotmail.com>
2024-05-22 12:43:54 -0300

    1.16.3

--- Desafios/desafio_16.py
+++ Desafios/desafio_16.py
@@ -2,6 +2,13 @@
 #No qual você digitará a letra P para jogar Papel, a letra R para jogar Pedra e a letra T para jogar Tesoura.
 #Você jogará contra o computador e contabilizara o número de vitórias, empates e derrotas.
 
+import random
+
+list = ['pedra', 'papel', 'tesoura']
+
+ranlist = random.choice(list)
+print(ranlist)
+
 pedra = 'pedra'
 papel = 'papel'
 tesoura = 'tesoura'
@@ -9,7 +16,8 @@ tesoura = 'tesoura'
 entrada = input("Insira ")
 
 if entrada == pedra: 
-    if pedra > tesoura:
-        print("venceu")
-    elif pedra < papel:
-        print("perdeu")
+    if ranlist == "tesoura":
+        if pedra > tesoura:
+         print("venceu")
+        else:
+            print("perdeu")
