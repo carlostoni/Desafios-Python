be94550aa45998c9a7e7d0fd549aa8801ab3a8ff
carlostoni <carlos_toni_azevedo@hotmail.com>
2024-06-10 21:35:00 -0300

    1.17.1

---
+++ Desafios/desafio_17.py
@@ -0,0 +1,6 @@
+#Crie uma função que receba um nome, separe as 4 primeiras letras e complete com a palavra mon.
+#Ex: Daniel   Danimon
+
+entrada = str(input("Insira a palavra para separar ")[:3])
+
+print(f"{entrada}mom")
